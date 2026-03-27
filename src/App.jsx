7ed1f3811e0b4e76143a0c07b07791c0ce1a3713
Ethan <superteam@Ethans-Macbook-Pro.local>
2026-03-27 14:07:08 +0200

    added project and about pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import About from './pages/About'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return <h1>hello world</h1>
+
+function App() {
+  return (
+    <BrowserRouter basename="/">
+
+      <nav className="navbar">
+        <NavLink to="/" end>Projects</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
