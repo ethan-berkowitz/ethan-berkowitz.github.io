1b542efbb74e54cdd85893410d6548ab44b1006f
Ethan <superteam@Ethans-MBP.home>
2026-03-30 15:08:51 +0300

    changed font, added new images, and added sleepy cat card and icon

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,28 +7,37 @@ export default function Projects() {
         title="Arcade Baseball"
         image="/assets/arcade_baseball.png"
         type="Game - Sports"
-        description="Pinball-inspired baseball arcade game!"
+        description="Pinball-inspired baseball arcade game."
         link="/projects/my-game"
         typeIcon="/assets/arcade_baseball_icon.png"
-        bottomText="000 - Made with Unity"
+        bottomText="003 - Made with Unity"
       />
       <ProjectCard
         title="Pattern Recall"
         image="/assets/pattern_recall.png"
         type="Game - Memory"
-        description="Memorize a sequence of patterns!"
+        description="Memorize a sequence of patterns."
         link="/projects/another"
         typeIcon="/assets/pattern_recall_icon.png"
-        bottomText="001 - Made with Godot"
+        bottomText="004 - Made with Godot"
       />
       <ProjectCard
         title="Tower Archer"
         image="/assets/tower_archer.png"
         type="Game - Tower Defense"
-        description="Defend your castle against a never ending wave of enemies!"
+        description="Defend your castle against a never ending wave of enemies."
         link="/projects/another"
         typeIcon="/assets/tower_archer_icon.png"
         bottomText="002 - Made with GameMaker"
+      />
+      <ProjectCard
+        title="Sleepy Cat"
+        image="/assets/sleepy_cat.png"
+        type="Game - Idle Clicker"
+        description="Dream of fish and then eat those fish. Repeat."
+        link="/projects/another"
+        typeIcon="/assets/sleepy_cat_icon.png"
+        bottomText="001 - Made with GameMaker"
       />
     </div>
   )
