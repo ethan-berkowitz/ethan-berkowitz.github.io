8b020d7c597041a5da1071cc833c0dae4661bc59
Ethan <superteam@Ethans-MBP.home>
2026-04-03 11:51:08 +0300

    added new paper texture and borders, and overlaid the title and type boxes over the image and description boxes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,15 +2,17 @@ import { Link } from 'react-router-dom'
 
 export default function ProjectCard({title, image, type, typeIcon, description, link, bottomText}) {
     return (
-      <Link to={link} className="project-card">
-        <h1>{title}</h1>
-        <img src={image} alt={title} />
-        <div className="project-card-type">
-          <h2>{type}</h2>
-          {typeIcon && <img src={typeIcon} alt={type} className="type-icon" />}
-        </div>
-        <p>{description}</p>
-        <h3>{bottomText}</h3>
-      </Link>
+      <div className="project-card-wrapper">
+        <Link to={link} className="project-card">
+          <h1>{title}</h1>
+          <img src={image} alt={title} />
+          <div className="project-card-type">
+            <h2>{type}</h2>
+            {typeIcon && <img src={typeIcon} alt={type} className="type-icon" />}
+          </div>
+          <p>{description}</p>
+        </Link>
+          <h3>{bottomText}</h3>
+      </div>
     )
 }
