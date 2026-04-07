57f955c02266b65832d9c9f3784d6a70c913bcff
Ethan <superteam@Ethans-MBP.home>
2026-04-07 12:12:18 +0300

    added social media icons at bottom of every page, removed background image

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import './App.css'
 function App() {
   return (
     <BrowserRouter basename="/">
-
       <nav className="navbar">
         <NavLink to="/" end>Projects</NavLink>
         <NavLink to="/about">About</NavLink>
@@ -16,7 +15,17 @@ function App() {
         <Route path="/" element={<Projects />} />
         <Route path="/about" element={<About />} />
       </Routes>
+
+      <footer className="icon-footer">
+        <a href="https://ethanberko.itch.io/" target="_blank" rel="noopener noreferrer">
+          <img src="/assets/itchio-logo-textless-black.png" alt="itch.io" className="icon_footer-img-link" />
+        </a>
+        <a href="https://github.com/ethan-berkowitz" target="_blank" rel="noopener noreferrer">
+          <img src="/assets/GitHub_Invertocat_Black.png" alt="github.com" className="icon_footer-img-link" />
+        </a>
+      </footer>
     </BrowserRouter>
+
   )
 }
 
---
+++ src/data/projectsData.js
@@ -0,0 +1,18 @@
+const projects = [
+  {
+    id: "arcade-baseball",
+    title: "Arcade Baseball",
+    image: "/assets/arcade_baseball.png",
+    type: "Game - Sports",
+    description: "Pinball-inspired baseball arcade game.",
+    typeIcon: "/assets/arcade_baseball_icon.png",
+    bottomText: "003 - Made with Unity",
+    // Project page specific fields:
+    fullDescription: "Longer description here...",
+    embedUrl: "https://itch.io/embed/...",
+    screenshots: ["/assets/arcade_baseball_1.png"],
+    tags: ["Unity", "C#", "Sports"],
+  },
+];
+
+export default projects;
